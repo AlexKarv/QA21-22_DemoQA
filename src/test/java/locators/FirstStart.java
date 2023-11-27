2c39ee6c6d557e56759bf41da0c8c9499e97ffed
AlexKarv <alex.testengineer@gmail.com>
2023-11-27 19:05:21 +0200

    Added firstClass

---
+++ src/test/java/locators/FirstStart.java
@@ -0,0 +1,21 @@
+package locators;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class FirstStart {
+    WebDriver driver;
+
+    @Test
+    public void firstStartTrello () {
+        driver = new ChromeDriver();
+        driver.get("https://demoqa.com/");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driver.quit();
+    }
+}
